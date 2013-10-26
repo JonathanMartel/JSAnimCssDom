105b3431b1c8725b815d7c81f48a9fb5f618a507
Jonathan Martel <jonathmartel@gmail.com>
2013-10-26 19:03:57 -0400

    Ajout du code de compatibilité du customEvent pour IE9 et +

--- src/anim.js
+++ src/anim.js
@@ -282,3 +282,17 @@ Anim.prototype.demarre = function(mode)
             clearTimeout(id);
         };
 }());
+
+//Source : https://developer.mozilla.org/fr/docs/Web/API/CustomEvent
+(function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   };
+
+  CustomEvent.prototype = window.CustomEvent.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
